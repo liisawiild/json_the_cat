819abc38b7292687d84408f0bf2227463e63fa05
Lisa Wild <lisakwild@gmail.com>
2024-02-22 07:43:52 -0800

    print error message if there is a request error

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,21 @@
 const request = require('request');
 
 const breedSearch = process.argv[2];
-console.log(breedSearch);
+// console.log(breedSearch);
 
-// let test = "[]";
-// console.log(test.length);
+// body is a string
+// let test = "[]";   == empty body
+// console.log(test.length); = 2
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`, (error, response, body) => {
-  if (body.length === 2) {
+  if (error) {
+    console.log(error);
+  } else if (body.length === 2) {
     console.log("Breed not found.");
-  }
-  if (body.length > 2) {
+  } else if (body.length > 2) {
     let data = JSON.parse(body);
     let breedDescription = data[0]["description"];
     console.log(breedDescription);
-}
+  }
 });
 
