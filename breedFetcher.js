8bd56463ed70283dfedceed2b029005e2a48f039
Lisa Wild <lisakwild@gmail.com>
2024-02-21 16:31:57 -0800

    add message 'Breed not found' if the breed is not in the database

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,8 +3,17 @@ const request = require('request');
 const breedSearch = process.argv[2];
 console.log(breedSearch);
 
+// let test = "[]";
+// console.log(test.length);
+
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`, (error, response, body) => {
-  let data = JSON.parse(body);
-  let breedDescription = data[0]["description"];
-  console.log(breedDescription);
+  if (body.length === 2) {
+    console.log("Breed not found.");
+  }
+  if (body.length > 2) {
+    let data = JSON.parse(body);
+    let breedDescription = data[0]["description"];
+    console.log(breedDescription);
+}
 });
+
