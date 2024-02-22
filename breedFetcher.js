ec8e91039e7768f0e565486c95347f962e14acf7
Lisa Wild <lisakwild@gmail.com>
2024-02-22 08:13:42 -0800

    require the breedSearch function from index.js, and pull the request function into a fetchBreedDescription function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,22 @@
 const request = require('request');
-
-const breedSearch = process.argv[2];
-// console.log(breedSearch);
+const { breedSearch } = require('./index');
 
 // body is a string
 // let test = "[]";   == empty body
 // console.log(test.length); = 2
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else if (body.length === 2) {
-    console.log("Breed not found.");
-  } else if (body.length > 2) {
-    let data = JSON.parse(body);
-    let breedDescription = data[0]["description"];
-    console.log(breedDescription);
-  }
-});
 
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedSearch}`, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else if (body.length === 2) {
+      console.log("Breed not found.");
+    } else if (body.length > 2) {
+      let data = JSON.parse(body);
+      let breedDescription = data[0]["description"];
+      console.log(breedDescription);
+    }
+  });
+
+};
