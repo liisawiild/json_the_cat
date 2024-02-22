bf993aa705be4d4350d14cdb770c131147966378
Lisa Wild <lisakwild@gmail.com>
2024-02-22 12:58:24 -0800

    lint code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,20 @@
 const request = require('request');
 const { breedName } = require('./index');
 
-let callback = (error, description) => {}
+
 const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-    request(url, (error, response, body) => {
-      if (error) {
-        callback(error, null);
-      } else if (body.length === 2) {
-        console.log("Breed not found.");
-        } else if (body.length > 2) {
-          let data = JSON.parse(body);
-          let breedDescription = data[0]["description"];
-          callback(null, breedDescription);
-          }
-    });
-  };
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else if (body.length === 2) {
+      console.log("Breed not found.");
+    } else if (body.length > 2) {
+      let data = JSON.parse(body);
+      let breedDescription = data[0]["description"];
+      callback(null, breedDescription);
+    }
+  });
+};
 
-module.exports = { fetchBreedDescription }
+module.exports = { fetchBreedDescription };
